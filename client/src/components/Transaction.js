cf183a99942d26c3bc6833f8868f5788c39cbdf0
rahuljindal18 <rahuljindal1989@gmail.com>
2020-03-28 17:10:59 +0530

    full stack

--- client/src/components/Transaction.js
+++ client/src/components/Transaction.js
@@ -11,7 +11,7 @@ const Transaction = ({ transaction }) => {
         {sign}${Math.abs(transaction.amount)}
       </span>
       <button
-        onClick={() => deleteTransaction(transaction.id)}
+        onClick={() => deleteTransaction(transaction._id)}
         className="delete-btn"
       >
         x
--- client/src/context/AppReducer.js
+++ client/src/context/AppReducer.js
@@ -1,16 +1,29 @@
 export default (state, action) => {
   switch (action.type) {
+    case "GET_TRANSACTIONS":
+      return {
+        ...state,
+        loading: false,
+        error: null,
+        transactions: action.payload
+      };
     case "DELETE_TRANSACTION":
       return {
         ...state,
         transactions: state.transactions.filter(
-          transaction => transaction.id !== action.payload
+          transaction => transaction._id !== action.payload
         )
       };
     case "ADD_TRANSACTION":
       return {
         ...state,
         transactions: [action.payload, ...state.transactions]
+      };
+    case "TRANSACTION_ERROR":
+      return {
+        ...state,
+        error: action.payload,
+        loading: false
       };
     default:
       return state;
